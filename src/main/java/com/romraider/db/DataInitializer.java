c85ea645930846a91eb4d35b807a3bab2c64dbb3
JuanMa Sierra <juanmgar@gmail.com>
2025-11-17 14:52:17 +0100

    Extracción de literales como constantes

--- src/main/java/com/romraider/db/DataInitializer.java
+++ src/main/java/com/romraider/db/DataInitializer.java
@@ -12,9 +12,24 @@ import java.util.Map;
 public class DataInitializer {
 
     private static final Logger logger = LoggerFactory.getLogger(DataInitializer.class);
+
     private static final String GAME_BOY = "Game Boy";
     private static final String GAME_BOY_ADVANCE = "Game Boy Advance";
     private static final String GENESIS = "Genesis";
+
+    private static final String NES_DIR = "/roms/nes/";
+    private static final String SNES_DIR = "/roms/snes/";
+    private static final String GB_DIR = "/roms/gb/";
+    private static final String GBA_DIR = "/roms/gba/";
+    private static final String GENESIS_DIR = "/roms/genesis/";
+    private static final String N64_DIR = "/roms/n64/";
+
+    private static final String EXT_NES = ".nes";
+    private static final String EXT_SFC = ".sfc";
+    private static final String EXT_GB = ".gb";
+    private static final String EXT_GBA = ".gba";
+    private static final String EXT_GEN = ".gen";
+    private static final String EXT_N64 = ".n64";
 
     public static void initializeWithDefaults() {
         EntityManager em = JpaUtil.getEntityManager();
@@ -55,9 +70,9 @@ public class DataInitializer {
                 {"Nintendo 64", ".n64,.z64,.v64", "n64"},
                 {"GameCube", ".iso,.gcm", "gamecube"},
                 {"Wii", ".iso,.wbfs", "wii"},
-                {"Game Boy", ".gb", "gb"},
+                {GAME_BOY, ".gb", "gb"},
                 {"Game Boy Color", ".gbc", "gbc"},
-                {"Game Boy Advance", ".gba", "gba"},
+                {GAME_BOY_ADVANCE, ".gba", "gba"},
                 {"Nintendo DS", ".nds", "nds"},
                 {"Nintendo 3DS", ".3ds,.cia", "3ds"},
                 {"Nintendo Switch", ".nsp,.xci", "switch"},
@@ -102,40 +117,40 @@ public class DataInitializer {
 
         Object[][] roms = {
                 // NES
-                {"Super Mario Bros.", "Clásico de plataformas de Nintendo.", null, "/roms/nes/super_mario_bros.nes", true, true, "NES"},
-                {"The Legend of Zelda", "Aventura de exploración y acción.", null, "/roms/nes/zelda.nes", true, false, "NES"},
-                {"Metroid", "Exploración alienígena con acción y plataformas.", null, "/roms/nes/metroid.nes", false, true, "NES"},
-                {"Castlevania", "Acción y horror gótico en 8 bits.", null, "/roms/nes/castlevania.nes", false, false, "NES"},
+                {"Super Mario Bros.", "Clásico de plataformas de Nintendo.", null, NES_DIR + "super_mario_bros" + EXT_NES, true, true, "NES"},
+                {"The Legend of Zelda", "Aventura de exploración y acción.", null, NES_DIR + "zelda" + EXT_NES, true, false, "NES"},
+                {"Metroid", "Exploración alienígena con acción y plataformas.", null, NES_DIR + "metroid" + EXT_NES, false, true, "NES"},
+                {"Castlevania", "Acción y horror gótico en 8 bits.", null, NES_DIR + "castlevania" + EXT_NES, false, false, "NES"},
 
                 // SNES
-                {"Super Mario World", "Colorido y amplio juego de plataformas.", null, "/roms/snes/super_mario_world.sfc", true, false, "SNES"},
-                {"Donkey Kong Country", "Plataformas con gráficos revolucionarios.", null, "/roms/snes/dkc.sfc", true, true, "SNES"},
-                {"Chrono Trigger", "JRPG de culto con viajes en el tiempo.", null, "/roms/snes/chrono_trigger.sfc", true, false, "SNES"},
-                {"Street Fighter II", "Clásico de lucha 2D.", null, "/roms/snes/sf2.sfc", false, true, "SNES"},
+                {"Super Mario World", "Colorido y amplio juego de plataformas.", null, SNES_DIR + "super_mario_world" + EXT_SFC, true, false, "SNES"},
+                {"Donkey Kong Country", "Plataformas con gráficos revolucionarios.", null, SNES_DIR + "dkc" + EXT_SFC, true, true, "SNES"},
+                {"Chrono Trigger", "JRPG de culto con viajes en el tiempo.", null, SNES_DIR + "chrono_trigger" + EXT_SFC, true, false, "SNES"},
+                {"Street Fighter II", "Clásico de lucha 2D.", null, SNES_DIR + "sf2" + EXT_SFC, false, true, "SNES"},
 
                 // Game Boy
-                {"Tetris", "El clásico puzzle por excelencia.", null, "/roms/gb/tetris.gb", false, true, "Game Boy"},
-                {"Pokémon Red", "Comienza tu viaje para atraparlos a todos.", null, "/roms/gb/pokemon_red.gb", true, false, "Game Boy"},
-                {"Kirby’s Dream Land", "Debut del personaje rosado favorito.", null, "/roms/gb/kirby.gb", false, false, "Game Boy"},
-                {"Dr. Mario", "Puzzle de cápsulas medicinales.", null, "/roms/gb/dr_mario.gb", false, true, "Game Boy"},
+                {"Tetris", "El clásico puzzle por excelencia.", null, GB_DIR + "tetris" + EXT_GB, false, true, GAME_BOY},
+                {"Pokémon Red", "Comienza tu viaje para atraparlos a todos.", null, GB_DIR + "pokemon_red" + EXT_GB, true, false, GAME_BOY},
+                {"Kirby’s Dream Land", "Debut del personaje rosado favorito.", null, GB_DIR + "kirby" + EXT_GB, false, false, GAME_BOY},
+                {"Dr. Mario", "Puzzle de cápsulas medicinales.", null, GB_DIR + "dr_mario" + EXT_GB, false, true, GAME_BOY},
 
                 // Game Boy Advance
-                {"Advance Wars", "Estrategia militar por turnos.", null, "/roms/gba/advance_wars.gba", false, false, "Game Boy Advance"},
-                {"Metroid Fusion", "Exploración y acción en un mundo alienígena.", null, "/roms/gba/metroid_fusion.gba", true, true, "Game Boy Advance"},
-                {"The Legend of Zelda: The Minish Cap", "Una aventura diminuta.", null, "/roms/gba/minish_cap.gba", true, false, "Game Boy Advance"},
-                {"Mario Kart: Super Circuit", "Velocidad y caos sobre ruedas.", null, "/roms/gba/mksc.gba", true, true, "Game Boy Advance"},
+                {"Advance Wars", "Estrategia militar por turnos.", null, GBA_DIR + "advance_wars" + EXT_GBA, false, false, GAME_BOY_ADVANCE},
+                {"Metroid Fusion", "Exploración y acción en un mundo alienígena.", null, GBA_DIR + "metroid_fusion" + EXT_GBA, true, true, GAME_BOY_ADVANCE},
+                {"The Legend of Zelda: The Minish Cap", "Una aventura diminuta.", null, GBA_DIR + "minish_cap" + EXT_GBA, true, false, GAME_BOY_ADVANCE},
+                {"Mario Kart: Super Circuit", "Velocidad y caos sobre ruedas.", null, GBA_DIR + "mksc" + EXT_GBA, true, true, GAME_BOY_ADVANCE},
 
                 // Genesis
-                {"Sonic the Hedgehog", "El erizo azul veloz.", null, "/roms/genesis/sonic.gen", false, true, "Genesis"},
-                {"Streets of Rage", "Beat 'em up callejero.", null, "/roms/genesis/sor.gen", false, false, "Genesis"},
-                {"Golden Axe", "Espada y brujería en scroll lateral.", null, "/roms/genesis/golden_axe.gen", true, false, "Genesis"},
-                {"Gunstar Heroes", "Run and gun frenético.", null, "/roms/genesis/gunstar_heroes.gen", false, true, "Genesis"},
+                {"Sonic the Hedgehog", "El erizo azul veloz.", null, GENESIS_DIR + "sonic" + EXT_GEN, false, true, GENESIS},
+                {"Streets of Rage", "Beat 'em up callejero.", null, GENESIS_DIR + "sor" + EXT_GEN, false, false, GENESIS},
+                {"Golden Axe", "Espada y brujería en scroll lateral.", null, GENESIS_DIR + "golden_axe" + EXT_GEN, true, false, GENESIS},
+                {"Gunstar Heroes", "Run and gun frenético.", null, GENESIS_DIR + "gunstar_heroes" + EXT_GEN, false, true, GENESIS},
 
                 // Nintendo 64
-                {"Super Mario 64", "Salto a las 3D de Mario.", null, "/roms/n64/super_mario_64.n64", true, true, "Nintendo 64"},
-                {"The Legend of Zelda: Ocarina of Time", "Una de las mejores aventuras jamás creadas.", null, "/roms/n64/ocarina.n64", true, true, "Nintendo 64"},
-                {"GoldenEye 007", "FPS multijugador mítico.", null, "/roms/n64/goldeneye.n64", false, true, "Nintendo 64"},
-                {"Banjo-Kazooie", "Plataformas 3D con humor y coleccionables.", null, "/roms/n64/banjo.n64", true, false, "Nintendo 64"},
+                {"Super Mario 64", "Salto a las 3D de Mario.", null, N64_DIR + "super_mario_64" + EXT_N64, true, true, "Nintendo 64"},
+                {"The Legend of Zelda: Ocarina of Time", "Una de las mejores aventuras jamás creadas.", null, N64_DIR + "ocarina" + EXT_N64, true, true, "Nintendo 64"},
+                {"GoldenEye 007", "FPS multijugador mítico.", null, N64_DIR + "goldeneye" + EXT_N64, false, true, "Nintendo 64"},
+                {"Banjo-Kazooie", "Plataformas 3D con humor y coleccionables.", null, N64_DIR + "banjo" + EXT_N64, true, false, "Nintendo 64"},
         };
 
         for (Object[] r : roms) {
